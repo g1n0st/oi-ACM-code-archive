c6ec5c9491661522fbed44f806932ff19fb6520a
zjqzyc <34199452+zjqzyc@users.noreply.github.com>
2018-01-28 13:52:55 +0800

    http://211.140.156.254:2333/problem/187

--- hhhoj/187.cpp
+++ hhhoj/187.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int V = 100010;
+const int V = 300010;
 typedef pair<int, int> PI;
 typedef pair<PI, PI> PII;
 char get() {
@@ -16,57 +16,74 @@ void Read(char *s, int &len) {
 	for (; !((c >= 'a' && c <= 'z') || c == '-'); c = get());
 	for (; ((c >= 'a' && c <= 'z') || c == '-'); s[len++] = c, c = get());
 }
-int Ans = 0;
+
+vector<PII> ce[V];
+vector<PI> dr[V]; int vis[V], pos[V];
+int Ans;
 namespace SAM {
-	int c[V][26], fa[V], len[V], pT, rt, lst;
+	int len[V], fa[V], rt, pT;
+	PI c[V][26];
 	void Init() {
-		for (int i = 0; i <= pT; i++) {
-			memset(c[i], 0, sizeof c[i]);
-			fa[i] = len[i] = 0;
-		} pT = 0;
-		lst = rt = ++pT;
+		rt = ++pT;
+	}
+	void link(int p, int f) {
+		Ans += (len[p] - len[f]);
+		fa[p] = f;
 	}
 	void cut(int x) {
-		Ans -= len[x] - len[fa[x]];
+		Ans -= (len[x] - len[fa[x]]);
 		fa[x] = 0;
 	}
-	void link(int x, int y) {
-		Ans += len[x] - len[y];
-		fa[x] = y;
-	}
-	void Extend(int x) {
-		int cur = ++pT;
-		len[cur] = len[lst] + 1;
+	void Extend(int lst, int x, int id) {
+		int cur = ++pT; len[cur] = len[lst] + 1;
 		int p = lst;
-		while (p && !c[p][x]) c[p][x] = cur, p = fa[p];
+		while (p && vis[c[p][x].first]) c[p][x].first = id, c[p][x].second = cur, p = fa[p];
 		if (!p) link(cur, rt);
 		else {
-			int q = c[p][x];
+			int q = c[p][x].second;
 			if (len[q] == len[p] + 1) link(cur, q);
 			else {
+				dr[id].push_back(PI(fa[q], q));
 				++pT; len[pT] = len[p] + 1; fa[pT] = fa[q];
 				memcpy(c[pT], c[q], sizeof c[q]);
-				while (c[p][x] == q) c[p][x] = pT, p = fa[p];
-				 link(pT, fa[q]); cut(q);  link(q, pT); link(cur, pT);
+				while (!vis[c[p][x].first] && c[p][x].second == q) {
+					c[p][x].second = pT,
+						ce[id].push_back(make_pair(PI(p, x), PI(q, pT)));
+					p = fa[p];
+				}
+				link(pT, fa[q]); cut(q);
+				link(q, pT); link(cur, pT);
 			}
 		}
-		lst = cur;
+		pos[id] = cur;
+	}
+	void Del(int x) {
+		if (dr[x].size()) {
+			int q = dr[x][0].second, p = dr[x][0].first;
+			int pT = fa[q]; cut(q); cut(pos[x]); cut(pT); link(q, p);
+			memcpy(c[pT], c[0], sizeof c[0]);
+			for (auto v : ce[x]) c[v.first.first][v.first.second].second = v.second.first;
+		}
+		else cut(pos[x]);
+		vis[x] = 1;
 	}
 }
-char s[V], f[V];
+char s[V];
 int L, pre[V];
 int main() {
 	//freopen("in.txt", "r", stdin);
 	//freopen("out.txt", "w", stdout);
-	int i, j, top = 0;
-	Read(s + 1, L); 
+	int i, j, now = 0;
+	vis[0] = pos[0] = 1;
+	Read(s + 1, L);  SAM::Init();
 	for (int i = 1; i <= L; i++) {
-		if (s[i] != '-') f[++top] = s[i];
-		else {
-			f[top] = 0; top--;
+		if (s[i] != '-') {
+			pre[i] = now; now = i;
+			SAM::Extend(pos[pre[i]], s[i] - 'a', i);
 		}
-		SAM::Init(); Ans = 0;
-		for (int j = 1; j <= top; j++) SAM::Extend(f[j] - 'a');
+		else {
+			SAM::Del(now); now = pre[now];
+		}
 		printf("%d\n", Ans);
 	}
 	return 0;
