dbc0191dddc969616a7c4395f701a20fca845448
zjqzyc <34199452+zjqzyc@users.noreply.github.com>
2018-03-15 15:18:13 +0800

    http://211.140.156.254:2333/problem/216

---
+++ hhhoj/216.cpp
@@ -0,0 +1,21 @@
+#include "begonia.h"
+#include <bits/stdc++.h>
+using namespace std;
+const double INV2 = 1 / sqrt(2.);
+double A[][2] = { {INV2, INV2}, {INV2, -INV2} };
+int query_xor(int n, int t) {
+	vector<int> all;
+	for (int t = 0; t < 23; t++) {
+		for (int i = 0; i < n; i++) manipulate(A, i);
+		all.emplace_back(query());
+	}
+	for (int i = 1; i < (1 << n); i++) {
+		bool ojbk = 1;
+		for (auto u : all)
+			if (__builtin_popcount(i & u) & 1) {
+				ojbk = 0; break;
+			}
+		if (ojbk) return i;
+	}
+	return 233333;
+}
