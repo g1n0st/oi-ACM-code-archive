67726c4493d621c040b47786b4ce5f09cd878e81
zjqzyc <34199452+zjqzyc@users.noreply.github.com>
2018-03-22 19:17:48 +0800

    http://uoj.ac/problem/370

--- UOJ/370.cpp
+++ UOJ/370.cpp
@@ -1,31 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-const int V = 300010;
+const int V = 262145;
+char get() {
+	static char buf[V], *p1 = buf, *p2 = buf;
+	if (p1 == p2) {
+		p2 = (p1 = buf) + fread(buf, 1, V, stdin);
+		if (p1 == p2) return EOF;
+	}
+	return *p1++;
+}
 void Read(int &x) {
-	scanf("%d", &x);
+	x = 0; static char c;
+	for (; !isdigit(c); c = get());
+	for (; isdigit(c); x = x * 10 + c - '0', c = get());
 }
-int n, a[V];
-LL ans, f[V];
-LL dfs(int x) {
-	if (!x) return 0;
-	if (~f[x]) return f[x];
-	f[x] = 1LL << 60;
-	for (int i = 1; i <= n; i++) f[x] = min(f[x], dfs(x & a[i]) + (x & a[i]));
-	return f[x];
-}
+int S, all, n, a[V], f[V], v[V];
 int main() {
 	//freopen("in.txt", "r", stdin);
 	//freopen("out.txt", "w", stdout);
-	Read(n); int K = (1 << 18) - 1, tK = K;
+	S = (1 << 18) - 1;
+	Read(n);
 	for (int i = 1; i <= n; i++) {
-		Read(a[i]); K &= a[i];
+		Read(a[i]); S &= a[i]; all |= a[i];
 	}
-	for (int i = 1; i < (1 << 18); i <<= 1)
-		if (K & i) {
-			ans += (LL)i * n;
+	for (int i = 1; i <= n; i++) v[all ^ a[i]] = 1;
+	int bS = S ^ all;
+	for (int s = all; ; s = (s - 1) & all) {
+		for (int i = 0; i <= 18; i++) v[s] |= v[s | (1 << i)];
+		if (!s) break;
+	}
+	for (int i = 1; i <= bS; i++) {
+		if ((i & bS) ^ i) continue; f[i] = 1 << 30;
+		for (int j = i; ; j = (j - 1) & i) {
+			if (j >= f[i]) break;
+			if (v[i ^ j]) f[i] = min(f[i], f[j] + j);
+			if (!j) break;
 		}
-	memset(f, -1, sizeof f);
-	cout << ans + dfs(tK - K) << endl;
+	}
+	cout << (LL)S * n + f[bS] << endl;
 	return 0;
 }
